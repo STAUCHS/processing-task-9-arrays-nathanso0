b84ef884f39e6ab12bbf81030072524d36358d8c
nathanso0 <nathan.so1165@gmail.com>
2024-05-21 23:05:49 -0400

    Should Be Finished

--- Sketch.java
+++ Sketch.java
@@ -11,6 +11,12 @@ public class Sketch extends PApplet {
   boolean downPressedCharacter = false;
   boolean leftPressedCharacter = false;
   boolean rightPressedCharacter = false;
+  boolean downPressedSnow = false;
+  boolean upPressedSnow = false;
+  boolean GameEnd = false;
+  boolean mouseclick = false;
+  int Lives = 3;
+
   float characterY = 150;
   float characterX = 150;
 
@@ -31,6 +37,21 @@ public class Sketch extends PApplet {
     background(0);
     //draw snow
     snow();
+    if (Lives == 3) {
+      fill (225, 0, 0);
+      rect(width-20, 0, 20, 20);
+    }
+    if (Lives >= 2){
+      fill (225, 0, 0);
+      rect(width-40, 0 ,20, 20);
+    }
+    if (Lives >= 1) {
+      fill (225, 0, 0);
+      rect(width-60, 0, 20, 20);
+    }
+       if(Lives == 0){
+        GameEnd = true;
+      }
     //draw player character
     if (upPressedCharacter) {
       characterY--;
@@ -44,30 +65,49 @@ public class Sketch extends PApplet {
     if (rightPressedCharacter) {
       characterX++;
   }
-  fill(225,0,0);
+  fill(0,0,225);
   ellipse(characterX, characterY, 20, 20);
+  if (GameEnd == true){
+    background(0);
+    System.out.println("Game Over Buddy");
+  }
   }
 
   // All other defined methods are written below:
   public void snow(){
     for (int i = 0; i < snowX.length; i++){
       fill(255);
-    circle(snowX[i], snowY[i], snowDiameter);
+      circle(snowX[i], snowY[i], snowDiameter);
       snowY[i] += 1;
         //When snow hits ground resest
       if (snowY[i]> height){
         snowY[i] = 0;
         snowX[i] = random(width);
       }
+
+      // Edit later to make bln for mutli key pressing
       if (keyPressed) {
-        if (keyCode == DOWN) {
+        if (upPressedSnow) {
           snowY[i] += 1;
         }
-        else if (keyCode == UP) {
+        else if (downPressedSnow) {
           snowY[i] -= 0.5;
         }
       }
+      if(dist(characterX, characterY, snowX[i], snowY[i]) < snowDiameter/2){
+        Lives -= 1;
+        snowY[i] = 0;
+        snowX[i] = random(width);
+      } 
+      if(mouseclick == true){
+        if(dist(mouseX, mouseY, snowX[i], snowY[i]) < snowDiameter){
+          snowY[i] = 0;
+          snowX[i] = random(width);
+          mouseclick = false;
+        }
+      }
     }
+  
   }
   public void keyPressed() {
     if (key == 'w') {
@@ -82,9 +122,16 @@ public class Sketch extends PApplet {
     else if (key == 'd') {
       rightPressedCharacter = true;
     }
-  }
+    else if (keyCode == DOWN) {
+      downPressedSnow = true;
+    }
+    else if (keyCode == UP) {
+      upPressedSnow = true;
+      }
+    }
   
-  public void keyReleased() {
+  
+public void keyReleased() {
     if (key == 'w') {
       upPressedCharacter = false;
     }
@@ -97,5 +144,23 @@ public class Sketch extends PApplet {
     else if (key == 'd') {
       rightPressedCharacter = false;
     }
+    else if (keyCode == DOWN) {
+      downPressedSnow = false;
+    }
+    else if (keyCode == UP) {
+      upPressedSnow = false;
+    }
+}
+public void SnowHide(){
+    for(int i = 0; i < snowX.length; i++){
+      if (dist(characterX, characterY, snowX[i], snowY[i]) < snowDiameter){
+        snowY[i] = 0;
+        snowX[i] = random(width);
+      }
+    }
   }
+  public void MouseClicked() {
+    mouseclick = true;
+  } 
+
 }
